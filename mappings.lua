3a8c4d5bb00d3dc87683a9419a712c0c6825f43e
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-02 09:22:28 +0300

    refactor

--- mappings.lua
+++ mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Go to lsp definition
+map("n", "<leader>gd", ":Telescope lsp_definitions<CR>", { desc = "Go to definition", silent = true })
+
+-- Go to references
+map("n", "<leader>gr", ":Telescope lsp_references<CR>", { desc = "Go to references", silent = true })
