4af809f6d1decdf1e3a3a6b298c0647cb0e07bba
bulutcan99 <gocerbulutcan@gmail.com>
2025-02-07 13:17:22 +0300

    feat

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,7 +24,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
-        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+        {
+            "LazyVim/LazyVim",
+            opts = {
+                colorscheme = "eldritch",
+            },
+            import = "lazyvim.plugins",
+        },
         -- import/override with your plugins
         { import = "plugins" },
     },
@@ -37,7 +43,7 @@ require("lazy").setup({
         version = false, -- always use the latest git commit
         -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
-    install = { colorscheme = { "tokyonight", "habamax" } },
+    install = { colorscheme = { "eldritch" } },
     checker = {
         enabled = true, -- check for plugin updates periodically
         notify = false, -- notify on update
