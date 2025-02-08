ed58323ac0c4fd873710c571db537e49231c0660
bulutcan99 <gocerbulutcan@gmail.com>
2025-02-08 12:45:01 +0300

    feat]

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,29 +20,9 @@ end, { desc = "Go to next buffer" })
 keymap.set("n", "<Leader>bO", function()
     vim.cmd("1,$bd!") -- Delete all buffers
 end, { desc = "Save and close all buffers" })
-
--- Switch to the previous buffer
 keymap.set("n", "<S-tab>", function()
     vim.cmd("bprev")
 end, { desc = "Go to previous buffer" })
-
--- Window editing
-keymap.set("n", "<Leader>tc", "<C-w>c", { desc = "Close current window" }) -- Close the current window
-keymap.set("n", "<Leader>tl", "<C-w>w", { desc = "Switch to next window" }) -- Switch to the next window
-
--- Split Windows
-keymap.set(
-    "n",
-    "<Leader>tv",
-    ":vsplit<Return>",
-    { desc = "Vertical split window" }
-) -- Split window vertically
-keymap.set(
-    "n",
-    "<Leader>ths",
-    ":split<Return>",
-    { desc = "Horizontal split window" }
-) -- Split window horizontally
 
 -- Muren keymaps
 keymap.set("n", "<Leader>fm", function()
