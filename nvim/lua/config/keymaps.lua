be6e7556c2a2de3937173ed7951e8f9ebf69e210
bulutcan99 <gocerbulutcan@gmail.com>
2025-04-27 17:01:20 +0300

    feat: kill toggle term

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,3 +17,14 @@ end, { desc = "Go to previous buffer" })
 vim.keymap.set("n", "dar", function()
     vim.cmd("delm! | delm A-Z0-9")
 end, { desc = "Delete all a-z / A-Z marks" })
+
+-- Terminal
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+
+-- Don't yank on delete/change operations
+keymap.set({ "n", "v" }, "d", [["_d]], opts)
+keymap.set({ "n", "v" }, "D", [["_D]], opts)
+keymap.set({ "n", "v" }, "c", [["_c]], opts)
+keymap.set({ "n", "v" }, "C", [["_C]], opts)
+keymap.set({ "n", "v" }, "x", [["_x]], opts)
+keymap.set({ "n", "v" }, "X", [["_X]], opts)
--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,7 +2,10 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = function()
-        require("toggleterm").setup({
+        local toggleterm = require("toggleterm")
+        local Terminal = require("toggleterm.terminal").Terminal
+
+        toggleterm.setup({
             size = 10,
             open_mapping = [[<C-p>]],
             shading_factor = 2,
@@ -15,5 +18,35 @@ return {
                 },
             },
         })
+
+        -- CTRL+P ile toggle yapıyoruz
+        vim.keymap.set("n", "<C-p>", function()
+            toggleterm.toggle(1) -- 1 numaralı terminali aç/kapat
+        end, { noremap = true, silent = true })
+
+        -- Akıllı terminal kapatma fonksiyonu
+        local function smart_kill_terminal()
+            local buftype = vim.bo.buftype
+            if buftype == "terminal" then
+                -- Eğer şu an terminal buffer'ındaysan sadece onu kapat
+                vim.cmd("bdelete!")
+            else
+                -- Eğer terminalde değilsen tüm açık terminalleri kapat
+                local terms = require("toggleterm.terminal").get_all()
+                for _, term in ipairs(terms) do
+                    if term:is_open() then
+                        term:close()
+                    end
+                end
+            end
+        end
+
+        -- CTRL+A ile çalıştırıyoruz
+        vim.keymap.set(
+            "n",
+            "<C-a>",
+            smart_kill_terminal,
+            { noremap = true, silent = true }
+        )
     end,
 }
